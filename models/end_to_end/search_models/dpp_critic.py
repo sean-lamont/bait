6093b1585d3d945e091ae2a4d2c3757c1079c1d8
sean-lamont <sean.a.lamont@outlook.com>
2025-02-21 13:32:13 +1030

    Add progress to DPP critic model

--- models/end_to_end/search_models/dpp_critic.py
+++ models/end_to_end/search_models/dpp_critic.py
@@ -7,6 +7,8 @@ import ray
 from experiments.end_to_end.proof_node import InternalNode
 from models.end_to_end.search_models.search_models import Search
 
+from dppy.finite_dpps import FiniteDPP
+
 
 class DPPCritic(Search):
     """
@@ -15,7 +17,7 @@ class DPPCritic(Search):
 
     """
 
-    def __init__(self, critic_model, state_encoder):
+    def __init__(self, critic_model, state_encoder, num_filtered, max_candidates):
         super().__init__()
 
         # neural model to provide a score for the goals
@@ -26,6 +28,12 @@ class DPPCritic(Search):
 
         # map goal to score and embeddings for models
         self.state_data = {}
+
+        # number of goals chosen per search iteration
+        self.num_filtered = num_filtered
+        
+        # maximum number of states to consider in DPP (if None, select all) 
+        self.max_candidates = max_candidates
 
     def reset(self, root):
         self.__init__(self.critic_model)
@@ -39,23 +47,51 @@ class DPPCritic(Search):
 
             embedding = ray.get(self.state_encoder.encode.remote([self.root.goal]))
 
-            self.state_data[self.root.goal] = (state_data[0], embedding[0].cpu().numpy())
+            self.state_data[self.root.goal] = (score[0], embedding[0].cpu().numpy())
 
     def get_goals(self):
 
         # create DPP matrix from currently unexplored states, scaling each by score
 
+        # only take the top max_candidates valid states
+        valid_states = [(goal,score) for goal, (score, embedding) in self.state_data.items() if score > -math.inf]
 
-
-        max_goal = max(self.state_data, key=self.state_data.get)
-        chosen_node = self.nodes[max_goal]
-
-        if chosen_node.is_explored:
+        if not valid_states:
             return None
-        else:
-            # Only allow one exploration of each node
-            self.state_data[max_goal] = -math.inf
-            return [(chosen_node, self.state_data[max_goal])]
+
+
+        valid_states = sorted(valid_states, key = lambda x: x[1], reverse=True)[:self.max_candidates]
+
+        # todo could optimise? E.g. preallocate np array?
+        
+        # todo join np matrix
+        emb_matrix = [self.state_data[x[0]][1] * self.state_data[x[0]][0] for x in valid_states]
+
+
+        # likelihood matrix for DPP
+        emb_matrix = emb_matrix @ emb_matrix.T
+
+
+        try:
+            DPP = FiniteDPP('likelihood', **{'L': emb_matrix})
+
+            # rng = np.random.RandomState(1)
+            DPP.sample_exact_k_dpp(size=num_filtered, mode='KuTa12')  # ,rng=rng)
+            chosen_inds = DPP.list_of_samples[0]
+
+        except Exception as e:
+            logger.error(f"Error sampling from DPP: {e}")
+            # take the top num_filtered tactics if DPP fails
+            chosen_inds = [[i for i in range(self.num_filtered)]][0]
+
+
+        chosen_goals = [valid_states[i][0] for i in chosen_inds]
+
+        # Only allow one exploration of each node
+        [self.state_data[goal] = (-math.inf, None) for goal in chosen_goals]
+
+        return [self.nodes[goal] for goal in chosen_goals]
+
 
     def process_responses(self, responses):
         for response in responses:
@@ -65,6 +101,12 @@ class DPPCritic(Search):
                 # Don't search proved/explored/queued nodes
                 if isinstance(result_node, InternalNode) and result_node.goal not in self.nodes:
                     self.nodes[result_node.goal] = result_node
-                    self.state_data[result_node.goal] = ray.get(self.critic_model.run.remote([result_node.goal]))[0]
+
+                    # Initialise scores for root
+                    score = ray.get(self.critic_model.run.remote([result_node.goal]))
+
+                    embedding = ray.get(self.state_encoder.encode.remote([result_node.goal]))
+
+                    self.state_data[result_node.goal] = (score[0], embedding[0].cpu().numpy())
 
         return
